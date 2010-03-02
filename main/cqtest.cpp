8e449aacdc56d7a51b5f9b7334f9c2ead9138f3d
Rohit Chatterjee <strategy.chatterjee@gmail.com>
2010-03-02 22:27:08 +0530

    keep up-to-date with CqInput changes (formerly CqData)

--- main/cqtest.cpp
+++ main/cqtest.cpp
@@ -1,16 +1,24 @@
 #include <cstdio>
 #include <fstream>
 
-#include "io/cqdata.h"
+#include "io/cqinput.h"
 
 int main(int argc, char* argv[])
 {
   if(argc < 2) {
+    printf("usage: %s <input file>\n", argv[0]);
     return 1;
   }
   printf("reading input file %s\n", argv[1]);
-  CqData cqdata(4901, 800);
-  std::ifstream ifs(argv[1]);
-  const size_t nread = cqdata.readData(ifs);
-  printf("read %d numbers\n", (int)nread);
+  CqInput cqinput(argv[1]);
+  if(!cqinput.valid()) {
+    printf("read failed\n");
+    return 1;
+  }
+  printf("cq dimension is %d\n", (int)cqinput.las().dim);
+  printf("created %d vectors\n", (int)cqinput.las().n_vectors());
+
+  printf("norm of element 0 is %d\n", (int)cqinput.las().norm(0));
+
+  return 0;
 }
