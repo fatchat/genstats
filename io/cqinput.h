e90fb5489a7131c719ec3cf57b00e734b84bfead
Rohit Chatterjee <strategy.chatterjee@gmail.com>
2010-03-02 22:26:10 +0530

    renamed cqdata.h don't use streams for file reading

---
+++ io/cqinput.h
@@ -0,0 +1,24 @@
+#ifndef CQINPUT_H
+#define CQINPUT_H
+
+#include "mathutils/linalg.h"
+
+class CqInput {
+  typedef short input_type;
+  typedef LinAlgSys<input_type> LAS;
+  LAS las_;
+  bool valid_;
+
+ public:
+  CqInput(const char* infile);
+  const LAS& las() const { return las_; }
+  LAS& las_ref() { return las_; }
+  bool valid() const { return valid_; }
+
+ private:
+  bool readData_(const char*);
+};
+
+size_t get_dim(const char*);
+
+#endif // CQINPUT_H
