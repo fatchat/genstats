598dbcbee5c711c61bacc5b5fb6908a5cc4e0e7e
Rohit Chatterjee <strategy.chatterjee@gmail.com>
2010-03-09 18:24:57 +0530

    rewrote

---
+++ math/linalg.cpp
@@ -0,0 +1,128 @@
+#include <cassert>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+#include <cmath>
+
+#include "math/linalg.h"
+
+namespace LinAlg {
+
+  // ======================== System ========================
+  std::size_t System::create()
+  {
+    const size_t sz = dim_ * next_index_;
+    if(sz == store_.size()) {
+      store_.resize(2 * sz + dim_);
+    }
+    return next_index_++;
+  }
+
+  Vector System::vec(size_t n)
+  {
+    assert(n < n_vectors());
+    return Vector(n, *this);
+  }
+
+  System::const_iterator System::begin(size_t n) const
+  {
+    return &store_[n * dim_];
+  }
+  
+  System::iterator System::begin(size_t n)
+  {
+    return &store_[n * dim_];
+  }
+
+  // ========================= Vector =========================
+  Vector::Vector(size_t pos, System& las)
+    : pos_(pos)
+    , las_(las)
+  { }
+  
+  Vector::Vector(System& las)
+    : pos_(las.create())
+    , las_(las)
+  { }
+
+  Vector::Vector(const Vector& v)
+    : pos_(v.las_.create())
+    , las_(v.las_)
+  {
+    std::copy(v.begin(), v.end(), this->begin());
+  }
+
+  System::const_element_ref Vector::operator[](size_t n) const
+  {
+    assert(n < las_.dim());
+    return *(las_.begin(pos_) + n);
+  }
+  
+  System::element_ref Vector::operator[](size_t n)
+  {
+    assert(n < las_.dim());
+    return *(las_.begin(pos_) + n);
+  }
+
+  System::const_iterator Vector::begin() const
+  {
+    return las_.begin(pos_);
+  }
+
+  System::iterator Vector::begin()
+  {
+    return las_.begin(pos_);
+  }
+
+  System::const_iterator Vector::end() const
+  {
+    return las_.begin(pos_) + las_.dim();
+  }
+
+  System::iterator Vector::end()
+  {
+    return las_.begin(pos_) + las_.dim();
+  }
+
+  bool Vector::operator==(const Vector& rhs) const
+  {
+    if(&rhs != this) {
+      for(size_t i = 0; i < las_.dim(); ++i) {
+	if(this->operator[](i) != rhs[i]) {
+	  return false;
+	}
+      }
+    }
+    return true;
+  }
+
+  // ======================== free functions ========================
+  double innerprod(const Vector& lhs, const Vector& rhs)
+  {
+    return std::inner_product(lhs.begin(), lhs.end(), rhs.begin(), 0.0);
+  }
+
+  double norm(const Vector& v)
+  {
+    return sqrt(innerprod(v, v));
+  }
+
+  double norm(const System& s, size_t v_index)
+  {
+    double n = 0;
+    for(size_t i = 0; i < s.dim(); ++i) {
+      const double d = *(s.begin(v_index) + i);
+      n += d*d;
+    }
+    return sqrt(n);
+  }
+
+  Vector operator*(double f, const Vector& v)
+  {
+    Vector w(v);
+    std::transform(v.begin(), v.end(), w.begin(),
+		   std::bind2nd(std::multiplies<double>(), f));
+    return w;
+  }
+
+} // namespace
---
+++ math/linalg.h
@@ -0,0 +1,82 @@
+#ifndef LINALG_H
+#define LINALG_H
+
+#include <vector>
+
+namespace LinAlg {
+
+  class Vector;
+
+  // ============================ System  ==============================
+  class System {
+    
+    typedef double T;
+
+  public:
+    typedef T*       iterator;
+    typedef const T* const_iterator;
+    typedef T&       element_ref;
+    typedef const T& const_element_ref;
+
+  private:
+    const std::size_t dim_;
+    size_t next_index_;
+    std::vector<T> store_;
+    
+  public:
+    System(std::size_t dim)
+      : dim_(dim)
+      , next_index_(0)
+      { }
+    
+    size_t dim() const { return dim_; }
+    size_t n_vectors() const { return next_index_; }
+    
+    size_t create();
+    Vector vec(size_t);
+
+    const_iterator begin(size_t n) const;
+    iterator begin(size_t n);
+
+  private:
+    // no impl
+    System();
+    System(const System&);
+    System& operator=(const System&);
+
+  }; // System
+
+  // ============================ Vector  ==============================
+  class Vector {
+    size_t pos_;
+    System& las_;
+
+    // no impl
+    Vector();
+    Vector& operator=(const Vector&);
+  public:
+    Vector(size_t pos, System&);
+    Vector(System&);
+    Vector(const Vector&);
+
+    System::const_element_ref operator[](size_t n) const;
+    System::element_ref operator[](size_t n);
+
+    System::const_iterator begin() const;
+    System::iterator begin();
+
+    System::const_iterator end() const;
+    System::iterator end();
+
+    bool operator==(const Vector&) const;
+  }; // Vector
+
+  // ========================= free functions ============================
+  double norm(const Vector&);
+  double norm(const System&, size_t vector_index);
+  Vector operator*(double, const Vector&);
+  double innerprod(const Vector&, const Vector&);
+
+} // namespace LinAlg
+
+#endif // LINALG_H
