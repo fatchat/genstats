f4f07a1f0a7c492ac4b3bff18ae8a3a39256e84c
Rohit Chatterjee <strategy.chatterjee@gmail.com>
2010-03-15 19:08:26 +0530

    frequency, prob

--- math/buckets.h
+++ math/buckets.h
@@ -11,12 +11,15 @@ namespace Stat {
 
     LinAlg::System buckets_;
     bool created_;
+    size_t column_total_;
   public:
     Buckets(size_t dim);
     void create(size_t n);
     void count(const LinAlg::Vector&);
     void print(size_t, FILE*);
-    bool verify_sums() const;
+    bool verify_sums(bool die=false);
+    size_t frequency(size_t symbol, size_t position) const;
+    double prob(size_t symbol, size_t position);
   };
 
 } // namespace
