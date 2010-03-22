f12c351da58a5372ef6becaae9a17143cc1d54fd
Rohit Chatterjee <strategy.chatterjee@gmail.com>
2010-03-22 21:11:16 +0530

    better memory management

--- math/ptree.cpp
+++ math/ptree.cpp
@@ -1,4 +1,6 @@
 #include <string>
+#include <stdexcept>
+
 #include "math/ptree.h"
 
 namespace Stat {
@@ -9,99 +11,176 @@ namespace Stat {
     , prob_(0.0)
   { }
 
-  PTree::node::~node()
+  PTree::node::node(const node& n)
+    : children_(n.children_)
+    , count_(n.count_)
+    , prob_(n.prob_)
+  { }
+
+  PTree::node& PTree::node::operator=(const PTree::node& rhs)
   {
-    for(std::map<size_t, node*>::iterator it = children_.begin();
-	it != children_.end(); ++it)
-      {
-	if(it->second) {
-	  delete it->second;
-	  it->second = 0;
-	}
-      }
+    if(&rhs != this) {
+      children_ = rhs.children_;
+      count_ = rhs.count_;
+      prob_ = rhs.prob_;
+    }
+    return *this;
   }
 
-  PTree::node* PTree::node::child(size_t el)
+  bool PTree::node::operator==(const PTree::node& rhs) const
   {
-    if(!children_[el]) {
-      children_[el] = new node();
+    if(&rhs == this) {
+      return true;
     }
-    return children_[el];
+    return (count_ == rhs.count_ &&
+	    prob_ == rhs.prob_ &&
+	    children_ == rhs.children_);
+  }
+
+  PTree::node::ind_type PTree::node::get_child_ind(PTree::node::el_type el) const
+  {
+    const nodemap::const_iterator it = children_.find(el);
+    if(it == children_.end()) {
+      return 0;
+    }
+    assert(it->second); // root_ is never a child
+    return it->second;
+  }
+
+  void PTree::node::create_new_child(PTree::node::el_type el, PTree::node::ind_type next)
+  {
+    assert(next);
+    assert(children_.find(el) == children_.end());
+    children_[el] = next;
   }
 
   void PTree::node::set_prob(size_t total)
-  // never called on root node
   {
-    assert(count_ > 0);
+    assert(count_ > 0); // never called on root node
     assert(total >= count_);
     prob_ = count_ * 1.0 / total;
   }
 
-  void PTree::node::set_child_probs()
+  void PTree::node::set_child_probs(PTree::storage& store)
   {
     size_t total = 0;
 
-    typedef std::map<size_t, node*>::iterator iterator;
-    const iterator end = children_.end();
-
-    for(iterator it = children_.begin(); it != end; ++it)
+    for(nodemap::const_iterator it = children_.begin(); it != children_.end(); ++it)
       {
-	assert(it->second);
-	total += it->second->count();
+	total += store.get_node(it->second).count();
       }
 
-    for(iterator it = children_.begin(); it != end; ++it)
+    for(nodemap::iterator it = children_.begin(); it != children_.end(); ++it)
       {
 	assert(it->second);
-	it->second->set_prob(total);
-	it->second->set_child_probs();
+	store.get_node_ref(it->second).set_prob(total);
+	store.get_node_ref(it->second).set_child_probs(store);
       }
   }
 
-  PTree::node* PTree::node::add_child_count(size_t el)
+  void PTree::node::show(FILE* fp, size_t ind) const
   {
-    node* c = child(el);
-    assert(c);
-    c->inc();
-    return c;
+    if(fp) {
+      fprintf(fp, "node %d: count_=%d, prob_=%1.2f\n",
+	      (int)ind, (int)count_, prob_);
+      typedef nodemap::const_iterator iterator;
+      for(iterator it = children_.begin(); it != children_.end(); ++it) {
+	printf("node %d has child %d holding %d\n",
+	       (int)ind, (int)it->second, (int)it->first);
+      }
+      printf("end of node %d\n", (int)ind);
+    }
   }
 
-  void PTree::node::show_probs(FILE* fp, size_t level) const
+  void PTree::node::show_probs(FILE* fp, size_t level, const PTree::storage& store) const
   {
     if(!fp) return;
-    typedef std::map<size_t, node*>::const_iterator iterator;
-    const iterator end = children_.end();
     const std::string spacing(2 * level, ' ');
-    for(iterator it = children_.begin(); it != end; ++it)
+    for(nodemap::const_iterator it = children_.begin(); it != children_.end(); ++it)
       {
-	assert(it->second);
+	const node& n = store.get_node(it->second);
 	fprintf(fp, "%sP[V_%d = %d | branch] = %1.3f\n",
 		spacing.c_str(),
-		(int)level, (int)it->first, it->second->prob());
-	it->second->show_probs(fp, 1 + level);
+		(int)level, (int)it->first, n.prob());
+	n.show_probs(fp, 1 + level, store);
       }
+  }
+
+  // =========================== PTree::storage ==========================
+  size_t PTree::storage::next_index()
+  {
+    assert(next_ <= nodestore_.size());
+    if(next_ == nodestore_.size()) {
+      nodestore_.resize(next_ * 2 + 1);
+    }
+    assert(next_ < nodestore_.size());
+    return next_++;
+  }
+
+  PTree::node& PTree::storage::get_node_ref(size_t ind)
+  {
+    if(ind >= next_) {
+      throw std::runtime_error("bad index in PTree::storage::get_node_ref");
+    }
+    return nodestore_.at(ind);
+  }
+
+  const PTree::node& PTree::storage::get_node(size_t ind) const
+  {
+    if(ind >= next_) {
+      throw std::runtime_error("bad index in PTree::storage::get_node");
+    }
+    return nodestore_.at(ind);
+  }
+
+  void PTree::storage::show(FILE* fp) const
+  {
+    if(fp) {
+      for(size_t i = 0; i < next_; ++i) {
+	nodestore_.at(i).show(fp, i);
+      }
+    }
   }
 
   // ============================== PTree ================================
   PTree::PTree()
-    : root_(new node())
-  { }
-
-  PTree::~PTree()
   {
-    if(root_) {
-      delete root_;
-      root_ = 0;
-    }
+    root_ = storage_.next_index();
+    assert(root_ == 0);
   }
-  
+
   void PTree::add_vector(const LinAlg::ConstVector& cv)
   {
-    node* current = root_;
+    //printf("adding vector:");  LinAlg::print_int_vector(cv, stdout);
+    node::ind_type current_ind = root_;
+
     for(size_t i = 0; i < cv.dim(); ++i) {
-      assert(current);
-      current = current->add_child_count(cv[i]);
+      node::ind_type child_ind = storage_.get_node(current_ind).get_child_ind(cv[i]);
+      if(child_ind == 0) {
+	const node::ind_type next_ind = storage_.next_index();
+	storage_.get_node_ref(current_ind).create_new_child(cv[i], next_ind);
+	child_ind = storage_.get_node(current_ind).get_child_ind(cv[i]);
+	assert(child_ind == next_ind);
+      }
+      assert(child_ind);
+      storage_.get_node_ref(child_ind).inc();
+      current_ind = child_ind;
     }
+  }
+
+  double PTree::vector_prob(const LinAlg::VectorBase& cv) const
+  {
+    double prob = 1.0;
+    node::ind_type current_ind = root_;
+    for(size_t i = 0; i < cv.dim(); ++i) {
+      node::ind_type child_ind = storage_.get_node(current_ind).get_child_ind(cv[i]);
+      if(child_ind == 0) {
+	return 0.0;
+      }
+      prob *= storage_.get_node(child_ind).prob();
+      current_ind = child_ind;
+    }
+    return prob;
   }
 
   void PTree::add_vectors(const LinAlg::System& las)
@@ -114,14 +193,17 @@ namespace Stat {
 
   void PTree::calc_probs()
   {
-    assert(root_);
-    root_->set_child_probs();
+    storage_.get_node_ref(root_).set_child_probs(storage_);
   }
 
   void PTree::show_probs(FILE* fp) const
   {
-    assert(root_);
-    root_->show_probs(fp, 0);
+    storage_.get_node(root_).show_probs(fp, 0, storage_);
+  }
+
+  void PTree::show_storage(FILE* fp) const
+  {
+    storage_.show(fp);
   }
 
 } // Stat
--- math/ptree.h
+++ math/ptree.h
@@ -2,6 +2,7 @@
 #define PTREE_H
 
 #include <cstdio>
+#include <vector>
 #include <map>
 #include "math/linalg.h"
 
@@ -9,39 +10,67 @@ namespace Stat {
 
   class PTree {
 
+    class storage;
+    // ============================= node ===============================
     class node {
-      std::map<size_t, node*> children_;
+    public:
+      typedef int el_type;
+      typedef size_t ind_type;
+      typedef std::map<el_type, ind_type> nodemap;
+
+    private:
+      nodemap children_;
       size_t count_;
       double prob_;
 
     public:
+      // required for std::vector<node>
       node();
-      ~node();
+      node(const node&);
+      node& operator=(const node&);
+
+      bool operator==(const node&) const;
 
       size_t count() const { return count_; }
       double prob() const { return prob_; }
-      node* child(size_t el);
+      ind_type get_child_ind(el_type el) const;
 
       void inc() { ++count_; }
       void set_prob(size_t total_for_this_depth);
-      void set_child_probs();
-      node* add_child_count(size_t el);
+      void set_child_probs(storage&);
+      void create_new_child(el_type, ind_type);
 
-      void show_probs(FILE*, size_t level) const;
+      void show(FILE*, size_t) const;
+      void show_probs(FILE*, size_t level, const storage&) const;
+
     }; // node
 
-    node *root_;
+    // ============================= storage ===============================
+    class storage {
+      std::vector<node> nodestore_;
+      size_t next_;
+    public:
+      storage() : next_(0) { }
+      size_t next_index();
+      node& get_node_ref(size_t ind);
+      const node& get_node(size_t ind) const;
+      void show(FILE*) const;
+    }; // storage
+
+    // ============================= PTree ================================
+    storage storage_;
+    node::ind_type root_;
 
   public:
     PTree();
-    ~PTree();
-    
+
     void add_vector(const LinAlg::ConstVector&);
     void add_vectors(const LinAlg::System&);
     void calc_probs();
+    double vector_prob(const LinAlg::VectorBase&) const;
 
     void show_probs(FILE*) const;
-
+    void show_storage(FILE*) const;
   }; // PTree
 
 } // Stat
